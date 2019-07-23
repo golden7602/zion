9989df84e2baabaf75ce99565919a35f8229cbbc
golden7602 <419331959@qq.com>
2019-07-23 14:02:16 +0800

    增加设计 一些窗体

---
+++ Ui/Ui_FormUserLogin.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'e:\Zion\Ui\FormUserLogin.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(379, 194)
+        Dialog.setMinimumSize(QtCore.QSize(379, 194))
+        Dialog.setMaximumSize(QtCore.QSize(379, 194))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        Dialog.setFont(font)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.widget = QtWidgets.QWidget(Dialog)
+        self.widget.setMinimumSize(QtCore.QSize(0, 45))
+        self.widget.setMaximumSize(QtCore.QSize(16777215, 45))
+        self.widget.setStyleSheet("")
+        self.widget.setObjectName("widget")
+        self.label = QtWidgets.QLabel(self.widget)
+        self.label.setGeometry(QtCore.QRect(76, 9, 32, 32))
+        self.label.setMinimumSize(QtCore.QSize(32, 32))
+        self.label.setMaximumSize(QtCore.QSize(32, 32))
+        self.label.setSizeIncrement(QtCore.QSize(64, 64))
+        self.label.setBaseSize(QtCore.QSize(64, 64))
+        self.label.setText("")
+        self.label.setPixmap(QtGui.QPixmap("../res/Login_64px.net.png"))
+        self.label.setScaledContents(True)
+        self.label.setObjectName("label")
+        self.label_4 = QtWidgets.QLabel(self.widget)
+        self.label_4.setGeometry(QtCore.QRect(114, 9, 145, 32))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(20)
+        font.setBold(True)
+        font.setUnderline(False)
+        font.setWeight(75)
+        font.setStrikeOut(False)
+        self.label_4.setFont(font)
+        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_4.setObjectName("label_4")
+        self.verticalLayout.addWidget(self.widget)
+        self.line = QtWidgets.QFrame(Dialog)
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.verticalLayout.addWidget(self.line)
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setContentsMargins(20, -1, 20, -1)
+        self.gridLayout.setHorizontalSpacing(10)
+        self.gridLayout.setVerticalSpacing(15)
+        self.gridLayout.setObjectName("gridLayout")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 0, 0, 1, 1)
+        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit.setMinimumSize(QtCore.QSize(0, 25))
+        self.lineEdit.setObjectName("lineEdit")
+        self.gridLayout.addWidget(self.lineEdit, 0, 1, 1, 1)
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setObjectName("label_3")
+        self.gridLayout.addWidget(self.label_3, 1, 0, 1, 1)
+        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_2.setMinimumSize(QtCore.QSize(0, 25))
+        self.lineEdit_2.setInputMask("")
+        self.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.lineEdit_2.setPlaceholderText("")
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.gridLayout.addWidget(self.lineEdit_2, 1, 1, 1, 1)
+        self.verticalLayout.addLayout(self.gridLayout)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.horizontalLayout_2.addWidget(self.buttonBox)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "UserLogin"))
+        self.label_4.setText(_translate("Dialog", "User Login"))
+        self.label_2.setText(_translate("Dialog", "UserCode:"))
+        self.label_3.setText(_translate("Dialog", "Password:"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
--- lib/ZionWidgets/Customer.py
+++ lib/ZionWidgets/Customer.py
@@ -10,12 +10,12 @@ from lib.JPMvc.JPModel import JPTableViewModelEditForm
 from lib.JPFunction import JPDateConver, setButtonIcon
 
 
-class Form_Customer(Ui_Form):
+class Form_Customer(QWidget):
     def __init__(self, mainform):
         super().__init__()
-        self.Widget = QWidget()
-        self.setupUi(self.Widget)
-        mainform.addForm(self.Widget)
+        self.UI = Ui_Form()
+        self.UI.setupUi(self)
+        mainform.addForm(self)
         self.SQL = """
             select 
                 fCustomerID as `ID`, 
@@ -29,17 +29,39 @@ class Form_Customer(Ui_Form):
                 fWeb as `主页Web`, 
                 fEndereco as `地址Endereco`, 
                 fFax as `传真Fax` 
-            from  t_customer
+            from  t_customer 
         """
-        self.dataInfo = JPTabelFieldInfo(self.SQL)
-        self.mod = JPTableViewModelEditForm(self.tableView, self.dataInfo)
-        self.tableView.setModel(self.mod)
-        self.tableView.resizeColumnsToContents()
+        
+        self.on_CMDREFRESH_clicked()
+
+    def on_CMDREFRESH_clicked(self):
+        cbo = self.UI.comboBox
+        cbo.DisabledEvent = True
+        sql = self.SQL if (
+            cbo.currentIndex() == -1
+        ) else self.SQL + " where fCustomerID={}".format(cbo.currentIndex())
+        self.dataInfo = JPTabelFieldInfo(sql)
+        self.mod = JPTableViewModelEditForm(self.UI.tableView, self.dataInfo)
+        self.UI.tableView.setModel(self.mod)
+        self.UI.tableView.resizeColumnsToContents()
+        lst = [[item.Datas[1], item.Datas[0]]
+               for item in self.dataInfo.RowsData]
+        cbo.setEditable(True)
+        for r in lst:
+            cbo.addItem(r[0], r[1])
+        cbo.setCurrentIndex(-1)
+        cbo.DisabledEvent = False
+
+    def on_comboBox_currentIndexChanged(self, index):
+        if self.UI.comboBox.DisabledEvent:
+            return
+        self.on_CMDREFRESH_clicked()
 
     def addButtons(self, btnNames: list):
         for item in btnNames:
             btn = QPushButton(item[0])
             btn.setObjectName(item[2].upper())
+            print(item[2].upper())
             setButtonIcon(btn)
-            self.horizontalLayout_Button.addWidget(btn)
-        QMetaObject.connectSlotsByName(self.Widget)
+            self.UI.horizontalLayout_Button.addWidget(btn)
+        QMetaObject.connectSlotsByName(self)
