6a4e2fe9f0fa1936fdb24dc95914d90eb5387121
golden7602 <419331959@qq.com>
2019-09-16 19:35:52 +0800

    3412

--- testttttt.py
+++ testttttt.py
@@ -1,4 +1,18 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtSql import QSqlDatabase
 
-print(Qt.Key_0)
-print(chr(Qt.Key_0))
+import ctypes
+
+
+class aa():
+    def __init__(self):
+        #ctypes.windll.LoadLibrary( 'C:/Users/Administrator.USER-20190111RX/AppData/Local/Programs/Python/Python37/Lib/site-packages/PyQt5/Qt/bin/libmysql.dll')
+        self.con2 = QSqlDatabase.addDatabase('QMYSQL')
+        self.con2.setHostName("127.0.0.1")
+        self.con2.setDatabaseName("myorder_python")
+        self.con2.setUserName("root")
+        self.con2.setPassword("1234")
+        self.con2.setPort(3366)
+        a = self.con2.open()
+
+
+aa()
